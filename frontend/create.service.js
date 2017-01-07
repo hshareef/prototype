2ac91c19c4575d20e8139850a84ce46182e67547
hshareef <hshareef@bu.edu>
2017-01-07 15:38:40 -0500

    Add files via upload

---
+++ frontend/create.service.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+createApp.service('CreateService',function(){
+	this.testCreateService = function(){
+		alert("create service works!");
+	};
+	
+	this.saveClaim = function(){
+		
+	};
+});
---
+++ frontend/createModule.js
@@ -0,0 +1,4 @@
+/**
+ * 
+ */
+var createApp = angular.module('CreateModule', []);
